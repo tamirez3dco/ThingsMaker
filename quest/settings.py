9bebc9a2eeff96d77a323bb5f6155cda0a7b5226
Amit Aviv <amit@ez3d.co>
2012-06-03 15:38:13 +0300

    ddd

--- quest/settings.py
+++ quest/settings.py
@@ -10,6 +10,7 @@ djcelery.setup_loader()
 
 DIRNAME = os.path.dirname(__file__)
 
+#DEBUG = True
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 TESTING = False
@@ -22,17 +23,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': '../quest.db',                      # Or path to database file if using sqlite3.
-#        'USER': '',                      # Not used with sqlite3.
-#        'PASSWORD': '',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
-
+#DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': '../quest.db', 'USER': '', 'PASSWORD': '', 'HOST': '', 'PORT': '',}}
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:

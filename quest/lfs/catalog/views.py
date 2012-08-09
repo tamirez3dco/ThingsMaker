59df4c9f7d131643a2e09ceb18d19450ff466036
Tamir <tamir@ez3d.co>
2012-08-09 13:10:44 +0000

    products_by_name page

--- quest/lfs/catalog/views.py
+++ quest/lfs/catalog/views.py
@@ -452,6 +452,25 @@ def all_products(request, template_name="lfs/catalog/products.html"):
         "products": raw_products
     }))
     return result
+
+def list_products_by_name(request, name, template_name="lfs/catalog/products/products_by_name.html"):
+    print ("name="+name)
+    raw_products = lfs.catalog.utils.get_products_by_name(name)
+    print ("raw_products.length="+str(len(raw_products)))
+    products = []
+    for product in raw_products:
+        products.append({
+            "image_url": product.get_item_image(),            
+            "name": product.name,
+            "price": product.price,
+            "product_url": "/product/" + product.slug
+        })
+    result = render_to_response(template_name, RequestContext(request, {
+        "name_of_p": 'amit',
+       "products": raw_products
+    }))
+    return result
+
     
 def product_view(request, slug, template_name="lfs/catalog/product_base.html"):
     """Main view to display a product.

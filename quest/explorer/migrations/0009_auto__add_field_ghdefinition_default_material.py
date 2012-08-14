123c7c02fc98a62bb5919f5959bb5a2c91d15e0c
Amit Aviv <amit@ez3d.co>
2012-08-14 09:39:07 +0300

    trying to fix migration..

--- quest/explorer/migrations/0009_auto__add_field_ghdefinition_default_material.py
+++ quest/explorer/migrations/0009_auto__add_field_ghdefinition_default_material.py
@@ -9,7 +9,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         
         # Adding field 'GhDefinition.default_material'
-        db.add_column('explorer_ghdefinition', 'default_material', self.gf('django.db.models.fields.related.ForeignKey')(default=0, to=orm['explorer.Material']), keep_default=False)
+        db.add_column('explorer_ghdefinition', 'default_material', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['explorer.Material']), keep_default=False)
 
 
     def backwards(self, orm):

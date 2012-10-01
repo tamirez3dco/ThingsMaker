7ce33b83a5bcd5f4df11e1f92283cc680155e0fc
Amit Aviv <amit@ez3d.co>
2012-10-02 01:24:51 +0200

    ss

--- quest/explorer/admin.py
+++ quest/explorer/admin.py
@@ -19,19 +19,22 @@ class GhDefinitionAdminForm( forms.ModelForm ):
     param_names = PickleField( max_length = 1000, required = True )
     accepts_text_params = forms.BooleanField(initial=False,required=False)
     default_material = forms.ModelChoiceField(Material.objects.all())
-
+    #use_cache = forms.BooleanField(initial=True,required=False)
     
 class GhDefinitionAdmin(admin.ModelAdmin):
-    fields = ('file_name','scene_file','product','active','param_names','accepts_text_params', 'default_material')
+    fields = ('file_name','scene_file','product','active','param_names','accepts_text_params', 'default_material','use_cache')
     form = GhDefinitionAdminForm
 #    def save_model(self, request, obj, form, change):
 #        obj.file_name = form.cleaned_data['file_name']
 #        obj.active = form.cleaned_data['active']
 #        obj.set_param_names(form.cleaned_data['param_names'])
 #        obj.save()    
-        
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id','definition')
+          
 admin.site.register(GhDefinition, GhDefinitionAdmin)
-admin.site.register(Item)
+admin.site.register(Item,ItemAdmin)
 admin.site.register(ExplorerConfig)
 admin.site.register(AppData)
 admin.site.register(DefinitionParam)

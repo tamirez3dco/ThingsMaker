b3c4cfd9a6c6e835e2c361fe88f84c46219dd3e4
Amit Aviv <amit@ez3d.co>
2012-11-01 23:29:10 +0200

    allow links to inactive products

--- quest/lfs/catalog/views.py
+++ quest/lfs/catalog/views.py
@@ -480,8 +480,8 @@ def product_view(request, slug, template_name="lfs/catalog/product_base.html"):
     """
     product = lfs_get_object_or_404(Product, slug=slug)
 
-    if (request.user.is_superuser or product.is_active()) == False:
-        raise Http404()
+    #if (request.user.is_superuser or product.is_active()) == False:
+    #    raise Http404()
 
     # Store recent products for later use
     recent = request.session.get("RECENT_PRODUCTS", [])
